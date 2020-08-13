e5d79f76a083c2edf6269cfd255ac0e58c4e2fd6
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-12 23:45:46 -0700

    added files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlus, faChevronLeft, faTrashAlt, faCheckDouble } from '@fortawesome/free-solid-svg-icons'
+import { BrowserRoueter } from 'react-router-dom'
+import 'typeface-roboto'
+import './index.css'
 import './index.css';
-import App from './App';
+import App from '../App/App';
+
+library.add(faPlus, faChevronLeft, faTrashAlt, faChackDouble)
 
 
 ReactDOM.render(
-    <App />,
+  <BrowserRoueter>
+    <App />
+  </BrowserRoueter>,
   document.getElementById('root')
 );
 
